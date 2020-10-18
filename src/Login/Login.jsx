b4dedc59adaee4f0a239886485ca203b7b76b992
sunakane <gedyra@gmail.com>
2020-10-18 17:08:09 +0900

    ログイン機能(現在はどんな場合でもログイン可能)

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Form, FormGroup, Input, Button, Label } from "reactstrap";
+import { Container, Form, FormGroup, Input, Button, Label } from "reactstrap";
 
 const Login = () => {
   return (
-    <Form>
-      <FormGroup row>
-        <Label for="loginEmail">メールアドレス</Label>
-        <Input
-          type="email"
-          name="email"
-          id="loginEmail"
-          placeholder="email@example.com"
-        />
-      </FormGroup>
-      <FormGroup row>
-        <Label for="loginPassword">パスワード</Label>
-        <Input
-          type="password"
-          name="password"
-          id="loginPassword"
-          placeholder="パスワード"
-        />
-      </FormGroup>
-      <Button color="primary" tag={Link} to="/main">
-        ログイン
-      </Button>
-    </Form>
+    <Container>
+      <Form>
+        <FormGroup row>
+          <Label for="loginEmail">メールアドレス</Label>
+          <Input
+            type="email"
+            name="email"
+            id="loginEmail"
+            placeholder="email@example.com"
+          />
+        </FormGroup>
+        <FormGroup row>
+          <Label for="loginPassword">パスワード</Label>
+          <Input
+            type="password"
+            name="password"
+            id="loginPassword"
+            placeholder="パスワード"
+          />
+        </FormGroup>
+        <Button color="primary" tag={Link} to="/main">
+          ログイン
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
---
+++ src/User.js
@@ -0,0 +1,27 @@
+class User {
+  isLoggedIn = () => this.get("isLoggedIn") === "true";
+
+  // webストレージにkey: valueで登録
+  set = (key, value) => localStorage.setItem(key, value);
+
+  get = (key) => this.getLocalStorage(key);
+
+  getLocalStorage = (key) => {
+    const ret = localStorage.getItem(key);
+    return ret ? ret : null;
+  };
+
+  login = async (email, password) => {
+    // ログイン処理
+    this.set("isLoggedIn", true);
+    return true;
+  };
+
+  logout = async () => {
+    if (this.isLoggedIn()) {
+      this.set("isLoggedIn", false);
+    }
+  };
+}
+
+export default new User();

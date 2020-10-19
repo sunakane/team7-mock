c21776ab9e1fa8d70142d6406e5c11f923143f41
sunakane <gedyra@gmail.com>
2020-10-19 14:18:45 +0900

    axiosのラッパー関数apiを追加

--- src/User.js
+++ src/User.js
@@ -1,5 +1,6 @@
 import { Cookies } from "react-cookie";
 import axios from "axios";
+import api from "./api/api";
 
 const endpoint = process.env.REACT_APP_API_ENDPOINT;
 class User {
@@ -7,7 +8,14 @@ class User {
     this.cookies = new Cookies();
   }
 
-  isLoggedIn = () => this.get("isLoggedIn") === "true";
+  // TODO: サーバ側のセッションIDと一致しているかどうかを確認するべき
+  isLoggedIn = async () => {
+    // return this.get("isLoggedIn");
+    const res = await api.get("/is_login");
+    console.log(res);
+    // return res.data["is_login"];
+    return res.data !== null ? res.data.is_login : null;
+  };
 
   // webストレージにkey: valueで登録
   // set = (key, value) => localStorage.setItem(key, value);
@@ -25,21 +33,18 @@ class User {
   login = async (submit_id, password) => {
     // ログイン処理
     try {
-      await axios.post(
-        `${endpoint}/login`,
-        {
-          submit_id: submit_id,
-          password: password,
-        },
-        {
-          // withCredentials: true,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-      this.set("isLoggedIn", true);
+      const res = await api.post("/login", {
+        submit_id: submit_id,
+        password: password,
+      });
+      console.log(res.data);
+
+      const sessionId = res.data.session_id;
+      this.set("isLoggedIn", sessionId);
       return true;
     } catch (e) {
-      console.error(e);
+      const { status, statusText } = e.response;
+      console.error(`Error! HTTP Status:${status} ${statusText}`);
     }
   };
 
